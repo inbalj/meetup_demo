3ef8cb81f199a51ec58082b6feb44f9321152182
Inbal Jona <inbaljona@hotmail.com>
2022-04-23 12:53:53 +0300

    meetup react demo

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,32 @@
+import { useHistory } from 'react-router-dom';
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch('https://react--meetup-9f6e3-default-rtdb.europe-west1.firebasedatabase.app/meetu' +
+        'ps.json', {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    ).then(()=> {
+      history.replace('/');
+    }) ;
+  }
+
+  return (
+    <section>
+      <h1>New Meetups Page</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+    </section>
+  );
+
+}
+
+export default NewMeetupPage;
